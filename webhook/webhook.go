04326b295605e257b37a6b09ffdf34086a343d87
kaito2 <kaito2@kaito2noMacBook-Pro.local>
2022-07-30 22:05:43 +0900

    Comment out unknown types.

--- webhook/webhook.go
+++ webhook/webhook.go
@@ -22,10 +22,13 @@ type Body struct {
 		Homepage          string `json:"homepage"`
 		FeedURL           string `json:"feed_url"`
 		AdditionalDetails struct {
-			Language   string      `json:"language"`
-			Categories []string    `json:"categories"`
-			Authors    []string    `json:"authors"`
-			Copyright  interface{} `json:"copyright"`
+			Language string `json:"language"`
+			// NOTE: The type is not specified in the documentation.
+			// Categories []any `json:"categories"`
+			// NOTE: The type is not specified in the documentation.
+			// Authors []any `json:"authors"`
+			// NOTE: The type is not specified in the documentation.
+			Copyright *string `json:"copyright"`
 		} `json:"additional_details"`
 	} `json:"feed"`
 	NewEntries []struct {
